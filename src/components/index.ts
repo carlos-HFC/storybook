91e559c0459e1cc46e30e4e2cd160172e37758c6
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-24 18:10:04 -0300

    .

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './Alert'
+export * from './Button'
+export * from './Card'
+export * from './FormFloating'
+export * from './Input'
+export * from './RadioButton'
+export * from './Switch'
