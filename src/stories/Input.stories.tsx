c25a82971b38a8dfe916d1554f802a6343560bca
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-25 18:31:49 -0300

    Melhor forma de apresentar o componente e suas variações

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,69 +1,61 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import * as C from '../components'
+import { Input } from '../components'
 import * as T from '../@types'
 
 import '../css/styles.min.css'
 
 export default {
   title: 'Example/Input',
-  component: C.Input,
+  component: Input,
   parameters: {
     controls: {
       expanded: true
     }
   },
   argTypes: {
-    subline: {
-      control: false
-    },
-    password: {
-      control: false
-    },
-    value: {
-      control: false
-    },
     type: {
+      control: false
+    },
+    placeholder: {
       control: false
     },
   }
 } as Meta;
 
-const Template: Story<T.InputProps> = (args) => <div style={{ width: "50%", position: "relative" }}><C.Input {...args} /></div>
+const Template: Story<T.InputProps> = (args) => <div style={{ width: "50%", marginBottom: "1rem" }}><Input placeholder={`Input ${args.size}`} {...args} /></div>
+
+export const Inputs = () => (
+  <>
+    <Template size="normal" />
+    <Template size="normal" subline />
+    <Template size="normal" password />
+  </>
+)
 
 export const Default = Template.bind({});
 Default.args = {
   size: 'normal',
-  subline: false,
-  password: false,
-  value: 'Input text',
-  type: "text"
+  placeholder: 'Input placeholder'
 };
 
-export const Subline = Template.bind({});
+export const Subline = Template.bind({})
 Subline.args = {
   size: 'normal',
   subline: true,
-  password: false,
-  value: 'Input text',
-  type: "text"
+  placeholder: 'Input placeholder'
 };
+Subline.argTypes = {
+  subline: { control: false }
+}
 
 export const Password = Template.bind({});
 Password.args = {
   size: 'normal',
-  subline: true,
   password: true,
-  value: 'Input text',
-  type: 'password'
+  placeholder: 'Input placeholder'
 };
-
-export const PasswordShowText = Template.bind({});
-PasswordShowText.args = {
-  size: 'normal',
-  subline: true,
-  password: true,
-  value: 'Input text',
-  type: 'text'
-};
+Password.argTypes = {
+  password: { control: false }
+}
