626de0558320aa7f819f9b3d4bd638cafd7d66e9
Carlos Faustino <chfcchfc96@gmail.com>
2021-02-25 18:28:06 -0300

    Melhoramento do layout e inserção do state

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,20 +1,26 @@
+import { useState } from 'react'
 import { FaEye, FaEyeSlash } from 'react-icons/fa'
 import { InputProps } from '../@types'
 
 export const Input: React.FC<InputProps> = ({ size = 'normal', subline = false, password = false, ...props }) => {
-  const type = password && props.type === 'text' ? 'text' : password && props.type === 'password' ? 'password' : 'text'
+  const [type, setType] = useState('password')
+
   return (
     <>
-      <input className={['input', `input__${size}`].join(' ')} {...props} type={type} />
-      {subline && <span className="input__bd" />}
-      {password && props.type === 'password' && (
-        <div className="input__r__button">
-          <button><FaEye /></button>
-        </div>
+      {!password && (
+        <>
+          <input className={['input', `input__${size}`, props.className].join(' ')} {...props} />
+          {subline && <span className="input__bd" />}
+        </>
       )}
-      {password && props.type === 'text' && (
-        <div className="input__r__button">
-          <button><FaEyeSlash /></button>
+      {password && (
+        <div className="input__group">
+          <input className={['input password', `input__${size}`, props.className].join(' ')} {...props} type={type} />
+          {subline && <span className="input__bd" />}
+          {type === 'password'
+            ? <button onClick={() => setType('text')}><FaEye /></button>
+            : <button onClick={() => setType('password')}><FaEyeSlash /></button>
+          }
         </div>
       )}
     </>
