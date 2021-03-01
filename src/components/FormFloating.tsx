44b1a7715cef2c0e438eafaa518ca3fc9e3af997
Carlos Faustino <chfcchfc96@gmail.com>
2021-03-01 18:27:00 -0300

    Novas propriedades

--- src/components/FormFloating.tsx
+++ src/components/FormFloating.tsx
@@ -1,11 +1,11 @@
 import { Input } from '.'
 import { FloatingProps } from '../@types'
 
-export const FormFloating: React.FC<FloatingProps> = ({ label, size = 'normal', ...props }) => {
+export const FormFloating: React.FC<FloatingProps> = ({ label, validation = null, validationText = "Texto de validação", variant = 'normal', id, subline = false, ...props }) => {
   return (
-    <div className={["floating", `floating__${size}`].join(' ')}>
-      <Input {...props} placeholder={label} />
-      <label htmlFor={props.id}>{label}</label>
+    <div className={["floating", `floating__${variant}`, props.className].join(' ')} title={props.title}>
+      <Input validation={validation} validationText={validationText} placeholder={label} subline={subline} {...props} />
+      <label htmlFor={id}>{label}</label>
     </div>
   )
 }
