1765806f36e0dd25cbeb81f7dc58c7f78760b434
Carlos Faustino <chfcchfc96@gmail.com>
2021-03-02 18:44:00 -0300

    Opção do textarea

--- src/components/FormControl.tsx
+++ src/components/FormControl.tsx
@@ -1,13 +1,16 @@
-import { Input, Select } from ".";
-import { FormControlProps, InputProps, SelectProps } from "../@types";
+import { Input, Select, Textarea } from ".";
+import { FormControlProps, InputProps, SelectProps, TextareaProps } from "../@types";
 
-export const FormControl: React.FC<FormControlProps> = ({ label, id = `FormControl__${label}_${(Math.random() * 1000).toFixed(3)}`, variant = 'normal', select = false, validation = null, validationText = "Texto de validação", ...props }) => {
+export const FormControl: React.FC<FormControlProps> = ({ label, id = `FormControl__${label}_${(Math.random() * 1000).toFixed(3)}`, variant = 'normal', select = false, textarea=false, validation = null, validationText = "Texto de validação", ...props }) => {
   return (
     <div className={['form__control', `form__control__${variant}`, props.className].join(' ')} title={props.title} style={props.style}>
       <label htmlFor={id}>{label}</label>
-      {select
-        ? <Select id={id} validation={validation} validationText={validationText} multiple={props.multiple} {...props as SelectProps}>{props.children}</Select>
-        : <Input validation={validation} validationText={validationText} id={id} {...props as InputProps} />}
+      {select &&
+        <Select id={id} validation={validation} validationText={validationText} multiple={props.multiple} {...props as SelectProps}>
+          {props.children}
+        </Select>}
+      {textarea && <Textarea id={id} validation={validation} validationText={validationText} {...props as TextareaProps} />}
+      {!textarea && !select && <Input validation={validation} validationText={validationText} id={id} {...props as InputProps} />}
     </div>
   )
 }
