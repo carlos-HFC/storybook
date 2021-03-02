f3d36395ffd1888af0e24ff88968ac51448c9dee
Carlos Faustino <chfcchfc96@gmail.com>
2021-03-02 19:41:50 -0300

    .

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -1,6 +1,6 @@
 import { RadioProps } from '../@types'
 
-export const RadioButton: React.FC<RadioProps> = ({ id, label, name, type = 'radio', validation = null, validationText = "Texto de validação", ...props }) => {
+export const RadioButton: React.FC<RadioProps> = ({ id, label, name, validation = null, validationText = "Texto de validação", radio = false, checkbox = false, ...props }) => {
   let classes = 'check__input'
   classes += props.className ? ` ${props.className}` : ''
   classes += validation ? ` ${validation}` : ''
@@ -8,7 +8,10 @@ export const RadioButton: React.FC<RadioProps> = ({ id, label, name, type = 'rad
   return (
     <>
       <div className="check__inline" title={props.title}>
-        <input type={type} id={id} name={name} className={classes} {...props} />
+        {(!radio && !checkbox) || (radio && checkbox) || radio
+          ? <input type="radio" id={id} name={name} className={classes} {...props} />
+          : <input type="checkbox" id={id} name={name} className={classes} {...props} />
+        }
         <label htmlFor={id} className="check__label">{label}</label>
       </div>
       {validation === 'valid' && <p className="valid__text">{validationText}</p>}
