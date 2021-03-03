45463674f04f11129bffc4e8a5c7ea360e9f9241
Carlos Faustino <chfcchfc96@gmail.com>
2021-03-03 18:19:41 -0300

    Componente ProgressBar

---
+++ src/components/Progress.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react"
+import { ProgressProps } from "../@types"
+
+export const Progress: FC<ProgressProps> = ({ variant = "dark", width = 0, label, striped, animated, ...props }) => {
+  let classes = 'progress__bar'
+  classes += variant ? ` progress__bar-${variant}` : ''
+  classes += striped ? ` progress__bar-striped` : ''
+  classes += animated ? ` progress__bar-animated progress__bar-striped` : ''
+  classes += props.className ? ` ${props.className}` : ''
+
+  if (width > 100) width = 100
+  if (width < 0) width = 0
+
+  return (
+    <div className="progress" {...props}>
+      <div className={classes} style={{ width: `${width}%` }}>
+        {label && `${width}%`}
+      </div>
+    </div>
+  )
+}
