36fb68cfcc29c13c368104f2191c54a0ad863ec7
Carlos Faustino <chfcchfc96@gmail.com>
2021-03-10 18:18:54 -0300

    FormControl com a escolha do campo

--- src/components/FormControl.tsx
+++ src/components/FormControl.tsx
@@ -1,7 +1,7 @@
 import { Input, Select, Textarea } from ".";
 import { FormControlProps, InputProps, SelectProps, TextareaProps } from "../@types";
 
-export const FormControl: React.FC<FormControlProps> = ({ label, id = `FormControl__${label}_${(Math.random() * 1000).toFixed(3)}`, variant = 'normal', select = false, textarea=false, validation = null, validationText = "Texto de validação", ...props }) => {
+export const FormControl: React.FC<FormControlProps> = ({ label, id = `FormControl__${label}_${(Math.random() * 1000).toFixed(3)}`, variant = 'normal', field = 'input', validation = null, validationText = "Texto de validação", ...props }) => {
   let classes = 'form__control'
   classes += variant ? ` form__control__${variant}` : ''
   classes += props.className ? ` ${props.className}` : ''
@@ -9,12 +9,12 @@ export const FormControl: React.FC<FormControlProps> = ({ label, id = `FormContr
   return (
     <div className={classes} title={props.title} style={props.style}>
       <label htmlFor={id}>{label}</label>
-      {select &&
+      {field === 'select' &&
         <Select id={id} validation={validation} validationText={validationText} multiple={props.multiple} {...props as SelectProps}>
           {props.children}
         </Select>}
-      {textarea && <Textarea id={id} validation={validation} validationText={validationText} {...props as TextareaProps} />}
-      {!textarea && !select && <Input validation={validation} validationText={validationText} id={id} {...props as InputProps} />}
+      {field === 'textarea' && <Textarea id={id} validation={validation} validationText={validationText} {...props as TextareaProps} />}
+      {field === 'input' && <Input validation={validation} validationText={validationText} id={id} {...props as InputProps} />}
     </div>
   )
 }
